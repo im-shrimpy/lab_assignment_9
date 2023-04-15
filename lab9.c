47708eb40ed0275fea3b8b64d9ca0e9ceb067aaa
im-shrimpy <nadavmf1@gmail.com>
2023-04-14 21:20:27 -0400

    finished code :)

--- lab9.c
+++ lab9.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // RecordType
 struct RecordType
 {
-	int		id;
-	char	name;
-	int		order; 
+	int	id;
+	char name;
+	int	order; 
 };
 
 // Fill out this structure
-struct HashType
+typedef struct HashType
 {
-
-};
+	struct RecordType *data;
+	struct HashType *next;
+}thing;
 
 // Compute the hash function
-int hash(int x)
+int hash(int x,int tableSize)
 {
+	return x%tableSize;
 
 }
 
@@ -73,22 +77,69 @@ void printRecords(struct RecordType pData[], int dataSz)
 // skip the indices which are free
 // the output will be in the format:
 // index x -> id, name, order -> id, name, order ....
-void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
+void displayRecordsInHash(thing *array,int i)
 {
-	int i;
-
-	for (i=0;i<hashSz;++i)
+	thing *cur=array;
+	printf("index %d -> ",i);
+	while(cur!=NULL)
 	{
+		printf("%d, %c, %d -> ",cur->data->id,cur->data->name,cur->data->order);
+		cur=cur->next;
 		// if index is occupied with any records, print all
 	}
+	printf("NULL\n");
+}
+
+
+
+void insertTable(thing **hashTable, struct RecordType *record,int tableSize)
+{
+    int key = hash(record->id, tableSize);
+    thing *new = malloc(sizeof(thing));
+    if (new == NULL) {
+        printf("Cannot allocate memory\n");
+        exit(-1);
+    }
+    new->data = record;
+    new->next = NULL;
+    if (hashTable[key] == NULL) {
+        hashTable[key] = new;
+    } else {
+        thing *cur = hashTable[key];
+        while (cur->next != NULL) {
+            cur = cur->next;
+        }
+        cur->next = new;
+    }
 }
 
 int main(void)
 {
-	struct RecordType *pRecords;
-	int recordSz = 0;
+    struct RecordType *pRecords;
+    int recordSz = 0;
 
-	recordSz = parseData("input.txt", &pRecords);
-	printRecords(pRecords, recordSz);
-	// Your hash implementation
+    recordSz = parseData("input.txt", &pRecords);
+    printRecords(pRecords, recordSz);
+    // Your hash implementation
+    int TABLE_SIZE = 11;
+    thing **table = calloc(TABLE_SIZE, sizeof(thing *));
+    for (int i = 0; i < recordSz; i++) {
+        insertTable(table, pRecords + i,TABLE_SIZE);
+    }
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        displayRecordsInHash(table[i], i);
+    }
+    // Free dynamically allocated memory
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        thing *cur = table[i];
+        while (cur != NULL) {
+            thing *next = cur->next;
+            free(cur->data);
+            free(cur);
+            cur = next;
+        }
+    }
+    free(table);
+    free(pRecords);
+    return 0;
 }
